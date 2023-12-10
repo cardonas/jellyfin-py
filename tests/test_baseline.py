ec1464f858752b173cfefcf7c82dd6e78716d988
Steven Cardona <cardona.sj@gmail.com>
2023-12-10 08:19:35 -0600

    implement skeleton

---
+++ tests/test_baseline.py
@@ -0,0 +1,13 @@
+import pytest
+import jellyfin
+
+def test_version():
+    expected_version = jellyfin.__version__
+    filepath = "./src/jellyfin/__init__.py"
+    with open(filepath, 'r') as file:
+        line = file.readline()
+        version = line.split("=")[1].strip().strip('"')
+        assert version == expected_version, f"Expected version is {expected_version}, but got {version}"
+
+if __name__ == "__main__":
+    pytest.main()
